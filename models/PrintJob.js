a0ee97344037ece9c5795589b51d9f785ddd9fc7
Aryan Chavan <aryan@aryansdomain.com>
2022-09-27 09:57:05 +0530

    add print job model

---
+++ models/PrintJob.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const printJobSchema = mongoose.Schema(
+	{
+		buyer: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		volume: {
+			type: Number,
+			required: true,
+		},
+		objectUrl: {
+			type: String,
+			required: true,
+		},
+		address: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("PrintJob", printJobSchema);
